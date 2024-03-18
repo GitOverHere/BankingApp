9a0ff07c4dc667d0939f34e90a82a9593a6dff36
X <itsbinny@gmail.com>
2024-03-18 10:46:23 -0700

    fix

---
+++ resources/views/signin.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<title>Sign In</title>
+<link rel="stylesheet" type="text/css" src = "css/login.css">
+
+<meta name="title">Login to {Bank}</meta>
+<meta name="description">Login to {Bank} here </meta>
+<meta name = "tags">bank,checking,savings</html>
+
+
+</head>
+
+
+<body>
+
+
+
+<div class="backdrop">
+
+
+
+</div>
+
+
+<div class="loginarea">
+<div class="login">
+
+
+
+</div>
+
+<div class="sign">
+
+</div>
+
+
+<div class="reset" >
+
+<form submit ="/reset.php">
+ 
+<input name="email" type="email" required>
+
+ 
+
+</form>
+
+
+
+</div>
+
+
+
+</div>
+
+
+
+</body>
+
+
+
+</html>
